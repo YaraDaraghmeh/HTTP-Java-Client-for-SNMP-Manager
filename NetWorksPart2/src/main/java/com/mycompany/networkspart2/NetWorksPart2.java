0450adeaaf0558ab849b2d8301af54c01f71a76d
YaraDaraghmeh <96748356+YaraDaraghmeh@users.noreply.github.com>
2025-01-24 19:59:47 +0200

    Add files via upload

---
+++ NetWorksPart2/src/main/java/com/mycompany/networkspart2/NetWorksPart2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.networkspart2;
+
+/**
+ *
+ * @author Yara_2
+ */
+public class NetWorksPart2 {
+
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+}
